b6707487169255675c11ab17c9250865bcd16c4c
calvuzs3 <calvetti.luca@gmail.com>
2026-07-19 11:56:03 +0200

    1.5.1
    Sistemato il bug nel DataStore crittato, ora se la chiave è corrotta la rigenera.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         minSdk = 33
         targetSdk = 35
 
-        versionCode = 4
-        versionName = "1.5.0"
+        versionCode = 5
+        versionName = "1.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
